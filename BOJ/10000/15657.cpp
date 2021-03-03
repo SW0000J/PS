5106f181064364680de362929cc6bb997bd897dd
sw0000j <sw0000j@gmail.com>
2021-03-03 18:30:24 +0900

    [Add] - 15657 Answer

---
+++ BOJ/10000/15657.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+vector<int> v;
+int arr[9];
+int N;
+int M;
+
+void dfs(const int& start, const int& count) {
+	if (count == M) {
+		for (int i = 0; i < M; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+
+		return;
+	}
+
+	for (int i = start; i <= N; i++) {
+		arr[count] = v[i];
+		dfs(i, count + 1);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	cin >> N >> M;
+	v.resize(N + 1);
+
+	for (int i = 1; i <= N; i++) {
+		cin >> v[i];
+	}
+
+	sort(v.begin(), v.end());
+
+	dfs(1, 0);
+
+	return 0;
+}

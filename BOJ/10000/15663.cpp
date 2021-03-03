1b2621345fc6317e767db50ecad0a3803e538c11
sw0000j <sw0000j@gmail.com>
2021-03-03 18:51:25 +0900

    [Add] - 15663 Answer

---
+++ BOJ/10000/15663.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+vector<int> v;
+int arr[9];
+int ans[9];
+bool visited[9];
+int N;
+int M;
+
+void dfs(const int& count) {
+	if (count == M) {
+		for (int i = 0; i < M; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+
+		return;
+	}
+
+	int used = 0;
+
+	for (int i = 1; i <= N; i++) {
+		if (!visited[i] && used != v[i]) {
+			visited[i] = true;
+
+			arr[count] = v[i];
+			used = v[i];
+			dfs(count + 1);
+
+			visited[i] = false;
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cout.tie(NULL);
+	cin.tie(NULL);
+
+	cin >> N >> M;
+	v.resize(N + 1);
+
+	for (int i = 1; i <= N; i++) {
+		cin >> v[i];
+	}
+
+	sort(v.begin(), v.end());
+
+	dfs(0);
+
+	return 0;
+}
